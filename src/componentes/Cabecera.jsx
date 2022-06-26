2dd4d342f3b826772ff6d1cecf5095f418789c7b
Francisco Reyes <viatechcenter@gmail.com>
2022-06-26 12:33:56 -0500

    Titulo del proyecto cambiado

--- src/componentes/Cabecera.jsx
+++ src/componentes/Cabecera.jsx
@@ -2,7 +2,7 @@ export default function Cabecera() {
   return (
     <>
       <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-        Citas Pacientes {""}
+        Consultorio {""}
         <span className="text-indigo-600">Veterinaria</span>
       </h1>
     </>
